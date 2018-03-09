b592f842614b15dd353d0b12020e445b5016bce2
James Rutledge <jarutledge2@gmail.com>
2018-03-09 16:01:46 -0500

    testing setup

---
+++ test/index.js
@@ -0,0 +1,26 @@
+import http from 'http'
+import chai from 'chai'
+
+// import '../src/server.js'
+
+
+describe("example test description", () => {
+
+  it("example test item", () => {
+
+    chai.expect(true).to.be.true;
+
+  })
+
+})
+
+
+// describe('Example Node Server', () => {
+//   it('should return 200', done => {
+//     http.get('http://127.0.0.1:3030', res => {
+//       console.log('statusCode', res.statusCode)
+//       chai.expect(res.statusCode).to.equal(200);
+//       done();
+//     });
+//   });
+// });
