6fc7a428bb64e6e9e70c13f26ed7ffb1f1ceeff2
James Rutledge <jarutledge2@gmail.com>
2018-03-09 14:24:26 -0500

    init commit - much already done here

---
+++ README.md
@@ -0,0 +1,61 @@
+# express_sqlite_boil
+
+homemade boilerplate for Express/SQLite3 servers
+
+# Description
+
+
+
+# Technologies Used:
+
+ExpressJS + SQLite3 + Sequelize
+
+
+# Installation Instructions:
+
+First, clone this repo to a directory on your local machine and run `npm i`:
+
+```sh
+git clone git@github.com:jmz527/express_sqlite_boil.git
+cd express_sqlite_boil/
+npm i
+```
+
+# Commands:
+
+# Directory Tree:
+
+```sh
+
+```
+
+# Thanks
+
+http://docs.sequelizejs.com/manual/tutorial/migrations.html
+
+
+
+# Sequelize-CLI Steps
+
+```sh
+node_modules/.bin/sequelize init
+
+node_modules/.bin/sequelize model:generate --name User --attributes email:string,first_name:string,last_name:string,username:string,password:string
+
+node_modules/.bin/sequelize db:migrate
+
+node_modules/.bin/sequelize db:migrate:undo
+
+node_modules/.bin/sequelize seed:generate --name demo-user
+
+node_modules/.bin/sequelize db:seed:all
+
+node_modules/.bin/sequelize db:seed:undo
+
+node_modules/.bin/sequelize db:seed:undo:all
+
+
+
+
+sudo rm -rv config migrations models seeders db.development.sqlite node_modules package-lock.json
+```
---
+++ routes/users.js
@@ -0,0 +1,80 @@
+var models  = require('../models');
+var express = require('express');
+var router = express.Router();
+var bcrypt = require('bcryptjs');
+
+// allow CORS
+router.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+/* GET users listing. */
+router.get('/', (req, res, next) => {
+  models.User.findAll().then((users) => {
+    res.json({ users: users });
+  });
+});
+
+router.post('/create', (req, res) => {
+  models.User.create({
+    first_name: req.body.first_name,
+    last_name: req.body.last_name,
+    email: req.body.email,
+    username: req.body.username,
+    password: req.body.password
+  })
+  .then((m) => {
+    console.log('NEW USER ADDED TO DB!!!!!!');
+    res.json({ user_id: m.id });
+  });
+});
+
+// User.findOrCreate({where: {username: 'fnord'}, defaults: {job: 'something else'}}))
+
+router.post('/auth', (req, res) => {
+  models.User.findOne({
+    where: {
+      username: req.body.username
+    }
+  }).then((m) => {
+
+    if (bcrypt.compareSync(req.body.password, m.password)) {
+      console.log('AUTH USER SUCCESS!!!!!!');
+
+      res.json(m);
+    } else {
+      res.json(["ERROR"]);
+    }
+
+    
+  })
+})
+
+router.get('/id/:user_id', (req, res) => {
+  models.User.findOne({
+    where: {
+      id: req.params.user_id
+    }
+  }).then((m) => {
+    console.log('FOUND USER!!!!!!');
+    res.json(m)
+  })
+});
+
+router.post('/destroy', (req, res) => {
+  models.User.destroy({
+    where: {
+      id: req.body.user_id
+    }
+  }).then((m) => {
+    console.log('USER DELETED!!!!!!');
+    console.log(m);
+    res.json(m);
+
+    // res.redirect('/');
+  });
+});
+
+module.exports = router;
