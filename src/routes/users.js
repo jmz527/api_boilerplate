6cf53e298f48117ec8fbff49cade5c63843e486d
James Rutledge <jarutledge2@gmail.com>
2018-03-16 16:40:43 -0400

    users.js clean up

--- src/routes/users.js
+++ src/routes/users.js
@@ -13,13 +13,16 @@ router.use((req, res, next) => {
 
 /* GET users listing. */
 router.get('/', (req, res, next) => {
-  models.User.findAll().then((users) => {
-    console.log(`\x1b[33m%s\x1b[0m`, 'ALL USER!!!!!!')
-    res.json({ users: users })
+  models.User.findAll().then((users) => { console.log(`\x1b[33m%s\x1b[0m`, 'ALL USER!!!!!!')
+    res.json({ users })
   })
 })
 
 router.post('/create', (req, res) => {
+  /*
+   findOrCreate returns an array containing the object that was found or created and
+    a boolean that will be true if a new object was created and false if not.
+  */
   models.User.findOrCreate({ where: { username: req.body.username }, defaults: {
     first_name: req.body.first_name,
     last_name: req.body.last_name,
@@ -27,59 +30,23 @@ router.post('/create', (req, res) => {
     password: req.body.password
   }})
   .spread((user, created) => {
-    if (created) {
-      console.log(`\x1b[32m%s\x1b[0m`, 'NEW USER ADDED TO DB!!!!!!')
+    if (created) { console.log(`\x1b[32m%s\x1b[0m`, 'NEW USER ADDED TO DB!!!!!!')
       console.log(`\x1b[2m`, user.get({ plain: true }))
-      res.json({ created: created, user_id: user.id })
-    } else {
-      console.log(`\x1b[31m%s\x1b[0m`, 'USER ALREADY EXISTS!!!!')
-      res.json({ created: created })
+      res.json({ created, user_id: user.id })
+    } else { console.log(`\x1b[31m%s\x1b[0m`, 'USER ALREADY EXISTS!!!!')
+      res.json({ created })
     }
   })
 
   /*
-   findOrCreate returns an array containing the object that was found or created and
-    a boolean that will be true if a new object was created and false if not, like so:
-
-  [ {
-      username: 'sdepold',
-      job: 'Technical Lead JavaScript',
-      id: 1,
-      createdAt: Fri Mar 22 2013 21: 28: 34 GMT + 0100(CET),
-      updatedAt: Fri Mar 22 2013 21: 28: 34 GMT + 0100(CET)
-    },
-    true ]
-
    In the example above, the "spread" on line 32 divides the array into its 2 parts and
     passes them as arguments to the callback function, which treats them as "user" and "created" in this case.
   */
 })
 
-// router.post('/createOne', (req, res) => {
-//   models.User.create({
-//     first_name: req.body.first_name,
-//     last_name: req.body.last_name,
-//     email: req.body.email,
-//     username: req.body.username,
-//     password: req.body.password
-//   })
-//   .then((m) => {
-//     console.log(`\x1b[32m%s\x1b[0m`, 'NEW USER ADDED TO DB!!!!!!')
-//     // console.log(`\x1b[2m`, m)
-//     res.json({ user_id: m.id })
-//   })
-// })
-
-
 router.post('/auth', (req, res) => {
-  models.User.findOne({
-    where: {
-      username: req.body.username
-    }
-  }).then((m) => {
-    if (bcrypt.compareSync(req.body.password, m.password)) {
-      console.log(`\x1b[34m%s\x1b[0m`, 'AUTH USER SUCCESS!!!!!!')
-
+  models.User.findOne({ where: { username: req.body.username } }).then((m) => {
+    if (bcrypt.compareSync(req.body.password, m.password)) { console.log(`\x1b[34m%s\x1b[0m`, 'AUTH USER SUCCESS!!!!!!')
       res.json(m)
     } else {
       res.json(['ERROR'])
@@ -92,9 +59,7 @@ router.get('/id/:user_id', (req, res) => {
     where: {
       id: req.params.user_id
     }
-  }).then((m) => {
-    console.log(`\x1b[33m%s\x1b[0m`, 'FOUND USER!!!!!!')
-    // console.log(`\x1b[2m`, m)
+  }).then((m) => { console.log(`\x1b[33m%s\x1b[0m`, 'FOUND USER!!!!!!')
     res.json(m)
   })
 })
@@ -104,9 +69,7 @@ router.post('/destroy', (req, res) => {
     where: {
       id: req.body.user_id
     }
-  }).then((m) => {
-    console.log(`\x1b[31m%s\x1b[0m`, 'USER DELETED!!!!!!')
-    // console.log(`\x1b[2m`, m)
+  }).then((m) => { console.log(`\x1b[31m%s\x1b[0m`, 'USER DELETED!!!!!!')
     res.json(m)
 
     // res.redirect('/');
