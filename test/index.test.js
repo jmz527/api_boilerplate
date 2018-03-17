4843a7200e15a99d35f2eeae344e2193c9e122e0
James Rutledge <jarutledge2@gmail.com>
2018-03-17 17:40:49 -0400

    took out all semicolons

--- test/index.test.js
+++ test/index.test.js
@@ -4,9 +4,9 @@ import chai from 'chai'
 describe("The Server's / route", () => {
   it('returns 200', done => {
     axios.get('http://127.0.0.1:3030').then((res) => {
-      chai.expect(res.status).to.equal(200);
-      done();
+      chai.expect(res.status).to.equal(200)
+      done()
     })
-  });
-});
+  })
+})
 
--- test/users.test.js
+++ test/users.test.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 import chai from 'chai'
 
-var newUserID;
-const userID = 0;
+var newUserID
+const userID = 0
 const userPW = 'password527'
 const newUser = {
   first_name: 'TEST',
@@ -22,117 +22,117 @@ describe("The Server's /users route", () => {
   it('returns 200', () => {
     axios.get('http://127.0.0.1:3030/users')
       .then((res) => chai.expect(res.status).to.equal(200))
-  });
+  })
 
   it('returns a data object', () => {
     axios.get('http://127.0.0.1:3030/users')
       .then((res) => {
-        chai.expect(res.status).to.equal(200);
-        chai.expect(res.hasOwnProperty('data')).to.be.true;
-        chai.expect(res.data).to.be.an('object');
+        chai.expect(res.status).to.equal(200)
+        chai.expect(res.hasOwnProperty('data')).to.be.true
+        chai.expect(res.data).to.be.an('object')
       })
   })
 
   it('returns a data object with an array', () => {
     axios.get('http://127.0.0.1:3030/users')
       .then((res) => {
-        chai.expect(res.status).to.equal(200);
-        chai.expect(res.hasOwnProperty('data')).to.be.true;
-        chai.expect(res.data).to.be.an('object');
-        chai.expect(res.data.hasOwnProperty('users')).to.be.true;
-        chai.expect(res.data.users).to.be.an('array');
+        chai.expect(res.status).to.equal(200)
+        chai.expect(res.hasOwnProperty('data')).to.be.true
+        chai.expect(res.data).to.be.an('object')
+        chai.expect(res.data.hasOwnProperty('users')).to.be.true
+        chai.expect(res.data.users).to.be.an('array')
       })
   })
-});
+})
 
 describe("The Server's /users/id/0 route", () => {
   it('returns 200', () => {
     axios.get('http://127.0.0.1:3030/users/id/'+userID)
       .then((res) => chai.expect(res.status).to.equal(200))
-  });
+  })
 
 
   it('returns a data object', () => {
     axios.get('http://127.0.0.1:3030/users/id/'+userID)
       .then((res) => {
-        chai.expect(res.status).to.equal(200);
-        chai.expect(res.hasOwnProperty('data')).to.be.true;
-        chai.expect(res.data).to.be.an('object');
+        chai.expect(res.status).to.equal(200)
+        chai.expect(res.hasOwnProperty('data')).to.be.true
+        chai.expect(res.data).to.be.an('object')
       })
-  });
+  })
 
 
   it('returns a data object with correct user property names', () => {
     axios.get('http://127.0.0.1:3030/users/id/'+userID)
       .then((res) => {
-        chai.expect(res.status).to.equal(200);
-        chai.expect(res.hasOwnProperty('data')).to.be.true;
-        chai.expect(res.data).to.be.an('object');
-        chai.expect(res.data.hasOwnProperty('id')).to.be.true;
-        chai.expect(res.data.hasOwnProperty('first_name')).to.be.true;
-        chai.expect(res.data.hasOwnProperty('last_name')).to.be.true;
-        chai.expect(res.data.hasOwnProperty('username')).to.be.true;
-        chai.expect(res.data.hasOwnProperty('password')).to.be.true;
-        chai.expect(res.data.hasOwnProperty('createdAt')).to.be.true;
-        chai.expect(res.data.hasOwnProperty('updatedAt')).to.be.true;
+        chai.expect(res.status).to.equal(200)
+        chai.expect(res.hasOwnProperty('data')).to.be.true
+        chai.expect(res.data).to.be.an('object')
+        chai.expect(res.data.hasOwnProperty('id')).to.be.true
+        chai.expect(res.data.hasOwnProperty('first_name')).to.be.true
+        chai.expect(res.data.hasOwnProperty('last_name')).to.be.true
+        chai.expect(res.data.hasOwnProperty('username')).to.be.true
+        chai.expect(res.data.hasOwnProperty('password')).to.be.true
+        chai.expect(res.data.hasOwnProperty('createdAt')).to.be.true
+        chai.expect(res.data.hasOwnProperty('updatedAt')).to.be.true
       })
-  });
+  })
 
 
   it('returns a data object with correct user property types', () => {
     axios.get('http://127.0.0.1:3030/users/id/'+userID)
       .then((res) => {
-        chai.expect(res.status).to.equal(200);
-        chai.expect(res.hasOwnProperty('data')).to.be.true;
-        chai.expect(res.data).to.be.an('object');
-        chai.expect(res.data.hasOwnProperty('id')).to.be.true;
-        chai.expect(res.data.hasOwnProperty('first_name')).to.be.true;
-        chai.expect(res.data.hasOwnProperty('last_name')).to.be.true;
-        chai.expect(res.data.hasOwnProperty('username')).to.be.true;
-        chai.expect(res.data.hasOwnProperty('password')).to.be.true;
-        chai.expect(res.data.hasOwnProperty('createdAt')).to.be.true;
-        chai.expect(res.data.hasOwnProperty('updatedAt')).to.be.true;
-        chai.expect(res.data.id).to.be.an('number');
-        chai.expect(res.data.first_name).to.be.an('string');
-        chai.expect(res.data.last_name).to.be.an('string');
-        chai.expect(res.data.username).to.be.an('string');
-        chai.expect(res.data.password).to.be.an('string');
-        chai.expect(res.data.createdAt).to.be.an('string');
-        chai.expect(res.data.updatedAt).to.be.an('string');
+        chai.expect(res.status).to.equal(200)
+        chai.expect(res.hasOwnProperty('data')).to.be.true
+        chai.expect(res.data).to.be.an('object')
+        chai.expect(res.data.hasOwnProperty('id')).to.be.true
+        chai.expect(res.data.hasOwnProperty('first_name')).to.be.true
+        chai.expect(res.data.hasOwnProperty('last_name')).to.be.true
+        chai.expect(res.data.hasOwnProperty('username')).to.be.true
+        chai.expect(res.data.hasOwnProperty('password')).to.be.true
+        chai.expect(res.data.hasOwnProperty('createdAt')).to.be.true
+        chai.expect(res.data.hasOwnProperty('updatedAt')).to.be.true
+        chai.expect(res.data.id).to.be.an('number')
+        chai.expect(res.data.first_name).to.be.an('string')
+        chai.expect(res.data.last_name).to.be.an('string')
+        chai.expect(res.data.username).to.be.an('string')
+        chai.expect(res.data.password).to.be.an('string')
+        chai.expect(res.data.createdAt).to.be.an('string')
+        chai.expect(res.data.updatedAt).to.be.an('string')
       })
-  });
+  })
 
   it('returns a data object with a matching user id', () => {
     axios.get('http://127.0.0.1:3030/users/id/'+userID)
       .then((res) => {
-        chai.expect(res.status).to.equal(200);
-        chai.expect(res.hasOwnProperty('data')).to.be.true;
-        chai.expect(res.data).to.be.an('object');
-        chai.expect(res.data.hasOwnProperty('id')).to.be.true;
-        chai.expect(res.data.id).to.be.an('number');
-        chai.expect(res.data.id).to.equal(userID);
+        chai.expect(res.status).to.equal(200)
+        chai.expect(res.hasOwnProperty('data')).to.be.true
+        chai.expect(res.data).to.be.an('object')
+        chai.expect(res.data.hasOwnProperty('id')).to.be.true
+        chai.expect(res.data.id).to.be.an('number')
+        chai.expect(res.data.id).to.equal(userID)
       })
-  });
+  })
 
 
-});
+})
 
 describe("The Server's /users/create route", () => {
   it('returns 200 and { created: true, user_id: <id> }', done => {
 
     axios.post('http://127.0.0.1:3030/users/create', newUser)
       .then((res) => {
-        chai.expect(res.status).to.equal(200);
-        chai.expect(res.hasOwnProperty('data')).to.be.true;
-        chai.expect(res.data).to.be.an('object');
-        chai.expect(res.data.created).to.be.true;
-        chai.expect(res.data.user_id).to.be.a("number");
-        newUserID = res.data.user_id;
-        done();
+        chai.expect(res.status).to.equal(200)
+        chai.expect(res.hasOwnProperty('data')).to.be.true
+        chai.expect(res.data).to.be.an('object')
+        chai.expect(res.data.created).to.be.true
+        chai.expect(res.data.user_id).to.be.a("number")
+        newUserID = res.data.user_id
+        done()
       })
 
-  });
-});
+  })
+})
 
 
 describe("The Server's /users/create route", () => {
@@ -140,15 +140,15 @@ describe("The Server's /users/create route", () => {
 
     axios.post('http://127.0.0.1:3030/users/create', newUser)
       .then((res) => {
-        chai.expect(res.status).to.equal(200);
-        chai.expect(res.hasOwnProperty('data')).to.be.true;
-        chai.expect(res.data).to.be.an('object');
-        chai.expect(res.data.created).to.be.false;
-        done();
+        chai.expect(res.status).to.equal(200)
+        chai.expect(res.hasOwnProperty('data')).to.be.true
+        chai.expect(res.data).to.be.an('object')
+        chai.expect(res.data.created).to.be.false
+        done()
       })
 
-  });
-});
+  })
+})
 
 
 
@@ -156,93 +156,93 @@ describe("The Server's /users/auth route", () => {
 
   it('returns 200', done => {
     axios.post('http://127.0.0.1:3030/users/auth', { username: newUser.username, password: userPW }).then((res) => {
-      chai.expect(res.status).to.equal(200);
-      done();
+      chai.expect(res.status).to.equal(200)
+      done()
     })
-  });
+  })
 
   it('returns a data object', done => {
     axios.post('http://127.0.0.1:3030/users/auth', { username: newUser.username, password: userPW }).then((res) => {
-      chai.expect(res.status).to.equal(200);
-      chai.expect(res.hasOwnProperty('data')).to.be.true;
-      chai.expect(res.data).to.be.an('object');
-      done();
+      chai.expect(res.status).to.equal(200)
+      chai.expect(res.hasOwnProperty('data')).to.be.true
+      chai.expect(res.data).to.be.an('object')
+      done()
     })
-  });
+  })
 
 
   it('returns a data object with correct user property names', done => {
     axios.post('http://127.0.0.1:3030/users/auth', { username: newUser.username, password: userPW }).then((res) => {
-      chai.expect(res.status).to.equal(200);
-      chai.expect(res.hasOwnProperty('data')).to.be.true;
-      chai.expect(res.data).to.be.an('object');
-      chai.expect(res.data.hasOwnProperty('id')).to.be.true;
-      chai.expect(res.data.hasOwnProperty('first_name')).to.be.true;
-      chai.expect(res.data.hasOwnProperty('last_name')).to.be.true;
-      chai.expect(res.data.hasOwnProperty('username')).to.be.true;
-      chai.expect(res.data.hasOwnProperty('password')).to.be.true;
-      chai.expect(res.data.hasOwnProperty('createdAt')).to.be.true;
-      chai.expect(res.data.hasOwnProperty('updatedAt')).to.be.true;
-      done();
+      chai.expect(res.status).to.equal(200)
+      chai.expect(res.hasOwnProperty('data')).to.be.true
+      chai.expect(res.data).to.be.an('object')
+      chai.expect(res.data.hasOwnProperty('id')).to.be.true
+      chai.expect(res.data.hasOwnProperty('first_name')).to.be.true
+      chai.expect(res.data.hasOwnProperty('last_name')).to.be.true
+      chai.expect(res.data.hasOwnProperty('username')).to.be.true
+      chai.expect(res.data.hasOwnProperty('password')).to.be.true
+      chai.expect(res.data.hasOwnProperty('createdAt')).to.be.true
+      chai.expect(res.data.hasOwnProperty('updatedAt')).to.be.true
+      done()
     })
-  });
+  })
 
 
   it('returns a data object with correct user property types', done => {
     axios.post('http://127.0.0.1:3030/users/auth', { username: newUser.username, password: userPW }).then((res) => {
-      chai.expect(res.status).to.equal(200);
-      chai.expect(res.hasOwnProperty('data')).to.be.true;
-      chai.expect(res.data).to.be.an('object');
-      chai.expect(res.data.hasOwnProperty('id')).to.be.true;
-      chai.expect(res.data.hasOwnProperty('first_name')).to.be.true;
-      chai.expect(res.data.hasOwnProperty('last_name')).to.be.true;
-      chai.expect(res.data.hasOwnProperty('username')).to.be.true;
-      chai.expect(res.data.hasOwnProperty('password')).to.be.true;
-      chai.expect(res.data.hasOwnProperty('createdAt')).to.be.true;
-      chai.expect(res.data.hasOwnProperty('updatedAt')).to.be.true;
-      chai.expect(res.data.id).to.be.an('number');
-      chai.expect(res.data.first_name).to.be.an('string');
-      chai.expect(res.data.last_name).to.be.an('string');
-      chai.expect(res.data.username).to.be.an('string');
-      chai.expect(res.data.password).to.be.an('string');
-      chai.expect(res.data.createdAt).to.be.an('string');
-      chai.expect(res.data.updatedAt).to.be.an('string');
-      done();
+      chai.expect(res.status).to.equal(200)
+      chai.expect(res.hasOwnProperty('data')).to.be.true
+      chai.expect(res.data).to.be.an('object')
+      chai.expect(res.data.hasOwnProperty('id')).to.be.true
+      chai.expect(res.data.hasOwnProperty('first_name')).to.be.true
+      chai.expect(res.data.hasOwnProperty('last_name')).to.be.true
+      chai.expect(res.data.hasOwnProperty('username')).to.be.true
+      chai.expect(res.data.hasOwnProperty('password')).to.be.true
+      chai.expect(res.data.hasOwnProperty('createdAt')).to.be.true
+      chai.expect(res.data.hasOwnProperty('updatedAt')).to.be.true
+      chai.expect(res.data.id).to.be.an('number')
+      chai.expect(res.data.first_name).to.be.an('string')
+      chai.expect(res.data.last_name).to.be.an('string')
+      chai.expect(res.data.username).to.be.an('string')
+      chai.expect(res.data.password).to.be.an('string')
+      chai.expect(res.data.createdAt).to.be.an('string')
+      chai.expect(res.data.updatedAt).to.be.an('string')
+      done()
     })
-  });
+  })
 
 
   it('returns a data object with matching user property values', done => {
     axios.post('http://127.0.0.1:3030/users/auth', { username: newUser.username, password: userPW }).then((res) => {
-      chai.expect(res.status).to.equal(200);
-      chai.expect(res.hasOwnProperty('data')).to.be.true;
-      chai.expect(res.data).to.be.an('object');
-      chai.expect(res.data.hasOwnProperty('id')).to.be.true;
-      chai.expect(res.data.hasOwnProperty('first_name')).to.be.true;
-      chai.expect(res.data.hasOwnProperty('last_name')).to.be.true;
-      chai.expect(res.data.hasOwnProperty('username')).to.be.true;
-      chai.expect(res.data.hasOwnProperty('password')).to.be.true;
-      chai.expect(res.data.hasOwnProperty('createdAt')).to.be.true;
-      chai.expect(res.data.hasOwnProperty('updatedAt')).to.be.true;
-      chai.expect(res.data.id).to.be.an('number');
-      chai.expect(res.data.first_name).to.be.an('string');
-      chai.expect(res.data.last_name).to.be.an('string');
-      chai.expect(res.data.username).to.be.an('string');
-      chai.expect(res.data.password).to.be.an('string');
-      chai.expect(res.data.createdAt).to.be.an('string');
-      chai.expect(res.data.updatedAt).to.be.an('string');
-      chai.expect(res.data.id).to.equal(newUserID);
-      chai.expect(res.data.first_name).to.equal(newUser.first_name);
-      chai.expect(res.data.last_name).to.equal(newUser.last_name);
-      chai.expect(res.data.username).to.equal(newUser.username);
-      chai.expect(res.data.password).to.equal(newUser.password);
-      // chai.expect(res.data.createdAt).to.equal(newUser.createdAt);
-      // chai.expect(res.data.updatedAt).to.equal(newUser.updatedAt);
-      done();
+      chai.expect(res.status).to.equal(200)
+      chai.expect(res.hasOwnProperty('data')).to.be.true
+      chai.expect(res.data).to.be.an('object')
+      chai.expect(res.data.hasOwnProperty('id')).to.be.true
+      chai.expect(res.data.hasOwnProperty('first_name')).to.be.true
+      chai.expect(res.data.hasOwnProperty('last_name')).to.be.true
+      chai.expect(res.data.hasOwnProperty('username')).to.be.true
+      chai.expect(res.data.hasOwnProperty('password')).to.be.true
+      chai.expect(res.data.hasOwnProperty('createdAt')).to.be.true
+      chai.expect(res.data.hasOwnProperty('updatedAt')).to.be.true
+      chai.expect(res.data.id).to.be.an('number')
+      chai.expect(res.data.first_name).to.be.an('string')
+      chai.expect(res.data.last_name).to.be.an('string')
+      chai.expect(res.data.username).to.be.an('string')
+      chai.expect(res.data.password).to.be.an('string')
+      chai.expect(res.data.createdAt).to.be.an('string')
+      chai.expect(res.data.updatedAt).to.be.an('string')
+      chai.expect(res.data.id).to.equal(newUserID)
+      chai.expect(res.data.first_name).to.equal(newUser.first_name)
+      chai.expect(res.data.last_name).to.equal(newUser.last_name)
+      chai.expect(res.data.username).to.equal(newUser.username)
+      chai.expect(res.data.password).to.equal(newUser.password)
+      // chai.expect(res.data.createdAt).to.equal(newUser.createdAt)
+      // chai.expect(res.data.updatedAt).to.equal(newUser.updatedAt)
+      done()
     })
-  });
+  })
 
-});
+})
 
 
 describe("The Server's /users/update route", () => {
@@ -250,30 +250,30 @@ describe("The Server's /users/update route", () => {
 
     axios.post('http://127.0.0.1:3030/users/update/' + newUserID, updatedUser)
       .then((res) => {
-        chai.expect(res.status).to.equal(200);
-        done();
+        chai.expect(res.status).to.equal(200)
+        done()
       })
-  });
+  })
 
   it('updated the user properly', done => {
 
     axios.get('http://127.0.0.1:3030/users/id/' + newUserID).then((res) => { // console.log('status', res.status)
-      chai.expect(res.status).to.equal(200);
-      chai.expect(res.data.id).to.be.an('number');
-      chai.expect(res.data.id).to.equal(newUserID);
-      chai.expect(res.data.first_name).to.be.an('string');
-      chai.expect(res.data.first_name).to.equal(updatedUser.first_name);
-      chai.expect(res.data.last_name).to.be.an('string');
-      chai.expect(res.data.last_name).to.equal(updatedUser.last_name);
-      chai.expect(res.data.email).to.be.an('string');
-      chai.expect(res.data.email).to.equal(updatedUser.email);
-      chai.expect(res.data.username).to.be.an('string');
-      chai.expect(res.data.username).to.equal(updatedUser.username);
-      done();
+      chai.expect(res.status).to.equal(200)
+      chai.expect(res.data.id).to.be.an('number')
+      chai.expect(res.data.id).to.equal(newUserID)
+      chai.expect(res.data.first_name).to.be.an('string')
+      chai.expect(res.data.first_name).to.equal(updatedUser.first_name)
+      chai.expect(res.data.last_name).to.be.an('string')
+      chai.expect(res.data.last_name).to.equal(updatedUser.last_name)
+      chai.expect(res.data.email).to.be.an('string')
+      chai.expect(res.data.email).to.equal(updatedUser.email)
+      chai.expect(res.data.username).to.be.an('string')
+      chai.expect(res.data.username).to.equal(updatedUser.username)
+      done()
     })
 
   })
-});
+})
 
 
 
@@ -283,11 +283,11 @@ describe("The Server's /users/destroy route", () => {
 
     axios.post('http://127.0.0.1:3030/users/destroy', { user_id: newUserID })
       .then((res) => {
-        chai.expect(res.status).to.equal(200);
-        chai.expect(res.data).to.be.an('number');
-        chai.expect(res.data).to.equal(1);
-        done();
+        chai.expect(res.status).to.equal(200)
+        chai.expect(res.data).to.be.an('number')
+        chai.expect(res.data).to.equal(1)
+        done()
       })
 
-  });
-});
+  })
+})
