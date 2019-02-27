6f9337e51d03289464113bedc9f4f8aeb4a3cbed
james rutledge <jarutledge2@gmail.com>
2019-02-27 05:41:55 -0500

    rounded out todo testing

--- test/todos.test.js
+++ test/todos.test.js
@@ -1,10 +1,12 @@
 import axios from 'axios'
 import chai from 'chai'
 
-var newTodoID;
+var newTodoID, newTodoItemID;
 const todoID = 1;
 const newTodo = { title: 'TEST_TITLE' };
 const updatedTodo = { title: 'UPDATED_TITLE' };
+const newTodoItem = { content: 'TEST_CONTENT' };
+const updatedTodoItem = { content: 'UPDATED_CONTENT' };
 
 
 describe("The Server's /todos route", () => {
@@ -138,6 +140,62 @@ describe("Updating the Server's /todos route", () => {
   });
 });
 
+describe("Posting to the Server's /todos/:id:/items route", () => {
+  it('returns 201', done => {
+
+    axios.post('http://127.0.0.1:8000/api/todos/' + newTodoID + '/items', newTodoItem)
+      .then((res) => {
+        chai.expect(res.status).to.equal(201);
+        chai.expect(res.hasOwnProperty('data')).to.be.true;
+        chai.expect(res.data).to.be.an('object');
+        // chai.expect(res.data.created).to.be.true;
+        chai.expect(res.data.id).to.be.a("number");
+        newTodoItemID = res.data.id;
+        done();
+      });
+
+  })
+});
+
+
+describe("Updating the Server's /todos/:id:/items/:id: route", () => {
+  it('returns 200', done => {
+
+    axios.put('http://127.0.0.1:8000/api/todos/' + newTodoID + '/items/' + newTodoItemID, updatedTodoItem)
+      .then((res) => {
+        chai.expect(res.status).to.equal(200);
+        done();
+      });
+  });
+
+  it('updated the todoItem properly', done => {
+
+    axios.get('http://127.0.0.1:8000/api/todos/' + newTodoID).then((res) => {
+      chai.expect(res.status).to.equal(200);
+      chai.expect(res.data.todoItems).to.be.an('array');
+      chai.expect(res.data.todoItems.length).to.equal(1);
+      chai.expect(res.data.todoItems[0].content).to.be.an('string');
+      chai.expect(res.data.todoItems[0].content).to.equal(updatedTodoItem.content);
+      done();
+    });
+
+  });
+});
+
+
+describe("Deleting the Server's /todos/:id:/items/:id: route", () => {
+  it('returns 204', done => {
+
+    axios.delete('http://127.0.0.1:8000/api/todos/' + newTodoID + '/items/' + newTodoItemID)
+      .then((res) => {
+        chai.expect(res.status).to.equal(204);
+        done();
+      });
+
+  });
+});
+
+
 describe("Deleting using the Server's /todos route", () => {
   it('returns 200', done => {
 
@@ -148,19 +206,7 @@ describe("Deleting using the Server's /todos route", () => {
       });
 
   });
-
-  // it('attempting to fetch the todo now returns 404', done => {
-
-  //   axios.get('http://127.0.0.1:8000/api/todos/' + newTodoID)
-  //     .then((res) => {
-  //       chai.expect(res.status).to.equal(404);
-  //       done();
-  //     });
-
-  // });
-
 });
-
 
 
 
